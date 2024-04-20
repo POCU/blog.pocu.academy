66fa1a2adc8921f16d408e9870055a5e4197b0a2
Chris Yoon <78404624+chrisyoon01@users.noreply.github.com>
2024-04-19 17:44:09 -0700

    Restyled language links, and removed logic to set locale cookie

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,27 +1,14 @@
 (function() {
     "use strict";
 
-    const LANGUAGE_COOKIE_NAME = "POCU.blog.locale";
+    const LOCALE_COOKIE_NAME = "POCU.blog.locale";
     const LOCALE_ENGLISH = "en";
     const LOCALE_KOREAN = "ko";
 
     ready(() => {
-        let now = new Date();
-        let expiryDate = new Date(now.getFullYear() + 1, now.getMonth(), now.getDate());
-
         let languageSelect = document.getElementById("language-select");
-        let linkEnglish = document.getElementById("link-en");
-        let linkKorean = document.getElementById("link-ko");
 
-        linkEnglish.addEventListener("click", () => {
-            onLanguageLinkClicked(LOCALE_ENGLISH, expiryDate);
-        });
-
-        linkKorean.addEventListener("click", () => {
-            onLanguageLinkClicked(LOCALE_KOREAN, expiryDate);
-        });
-
-        let localeOrNull = getCookieValueOrNull(LANGUAGE_COOKIE_NAME);
+        let localeOrNull = getCookieValueOrNull(LOCALE_COOKIE_NAME);
         if (localeOrNull !== null) {
             let locale = localeOrNull;
 
@@ -53,11 +40,6 @@
         }
 
         return null;
-    }
-
-    function onLanguageLinkClicked(locale, expiryDate) {
-        document.cookie = `${LANGUAGE_COOKIE_NAME}=${locale}; SameSite=Lax; expires=${expiryDate.toGMTString()}; path=/;`;
-        window.location.href = `/${locale}`;
     }
 
     function ready(func) {
